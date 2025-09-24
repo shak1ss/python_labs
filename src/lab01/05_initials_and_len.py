470dddece2b0a9e61831d017dd5aeb54306a1df4
shakis <bulka5052@gmail.com>
2025-09-24 16:13:12 +0300

    labas07

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,7 +1,13 @@
-FIO = input("Введите полное ФИО: ")
-length = len(FIO.strip())
-parts = FIO.split()
-initials = "".join(word[0].upper() for word in parts) + "."
-print (f"ФИО: {FIO}\n"
-       f"Инициалы: {initials}\n"
-       f"Длина (символов): {length}")
+FIO = input("ФИО: ")
+while '  ' in FIO:
+    FIO = FIO.replace('  ', ' ')
+splitwords = FIO.split()
+FIO_2 = FIO.rstrip().lstrip()
+fletters = []
+str_fletters = ''
+for splitwords in splitwords:
+    fletters.append(splitwords[0])
+for letter in fletters:
+    str_fletters +=  letter
+print(f"Инициалы: {str_fletters}")
+print(f"Длина (символов): {len(FIO_2)}")
